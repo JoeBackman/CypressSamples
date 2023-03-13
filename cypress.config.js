dd0ec7fdd33fc944e60fb689e23c0d5ee9a03752
JBWebsolutions <Backman35@gmail.com>
2023-03-13 16:13:07 -0400

    updating config.js and form page objects

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportWidth: 1280,
+  viewportHeight: 720,
   env: {
     url: "https://www.ultimateqa.com/automation",
   },
--- cypress/support/pageObjects/UltiQaAutoForm.js
+++ cypress/support/pageObjects/UltiQaAutoForm.js
@@ -1,14 +1,36 @@
 class Form
 {
-
-    getFirstName(value){
-        const bn = cy.get('input[id="user[email]"]')
-        return bn.type(value)
+    //Left justified name field
+    getFirstNameLeft(value){
+        const bnl = cy.get('input[id="et_pb_contact_name_0"]').click({ force: true })
+        return bnl.type(value)
+    }
+    //Left justified message field
+    getMesageBoxLeft(value){
+        const bml = cy.get('textarea[id="et_pb_contact_message_0"]').click({ force: true })
+        return bml.type(value)
     }
 
-    getBusinessTin(value){
-        const bn = cy.get('input[id="business.tin"]')
-        return bTin.type(value)
+    //Right justified name field
+    getFirstNameRight(value){
+        const bnr = cy.get('input[id="et_pb_contact_name_1"]').click({ force: true })
+        return bnr.type(value)
+    }
+    
+    //Right justified message field
+    getMesageBoxRight(value){
+        const bmr = cy.get('textarea[id="et_pb_contact_message_1"]').click({ force: true })
+        return bmr.type(value)
+    }
+    
+    //Right justified submit button requires a simple math calculation, we handle that below
+    getRightSubmitButtonCaptcha(value){
+        const parts = questionText.split(' ')
+        const num1 = parseInt(parts[0])
+        const num2 = parseInt(parts[2])
+        const result = num1 + num2
+        const calc = cy.get('#et_pb_contact_captcha_0').type(result)
+        return calc.type(value)
     }
 
 }
